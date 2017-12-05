6861f5709c148be5eec3233f9a275071376f431f
John Chadwick <johnwchadwick@gmail.com>
2017-12-05 13:28:33 -0500

    Add some additional logging.

--- cmd/bztcp/main.go
+++ cmd/bztcp/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"time"
 
 	bztcp "github.com/Benzinga/go-bztcp"
 )
@@ -15,7 +16,9 @@ const defaultAddr = "tcp-v1-1.benzinga.com:11337"
 
 func main() {
 	var addr, user, key string
-	var tls bool
+	var tls, verbose bool
+
+	startTime := time.Now()
 
 	// Set up logging.
 	log.SetFlags(log.Lshortfile)
@@ -29,14 +32,24 @@ func main() {
 	flag.StringVar(&user, "user", "", "username to authenticate with")
 	flag.StringVar(&key, "key", "", "key to authenticate with")
 	flag.BoolVar(&tls, "tls", false, "whether or not to use TLS")
+	flag.BoolVar(&verbose, "v", false, "enable verbose logging")
 	flag.Parse()
 
+	if verbose {
+		log.Println("Benzinga TCP Client initializing.")
+	}
+
 	dialer := bztcp.Dial
+	tlsonoff := "off"
 	if tls {
 		dialer = bztcp.DialTLS
+		tlsonoff = "on"
 	}
 
 	// Dial server.
+	if verbose {
+		log.Printf("Connecting to %s as %s (TLS: %s)\n", addr, user, tlsonoff)
+	}
 	conn, err := dialer(addr, user, key)
 	if err != nil {
 		log.Fatalln(err)
@@ -47,10 +60,16 @@ func main() {
 		ch := make(chan os.Signal)
 		signal.Notify(ch, os.Interrupt)
 		<-ch
+		if verbose {
+			log.Println("Received signal. Exiting...")
+		}
 		cancel()
 	}()
 
 	// Start streaming.
+	if verbose {
+		log.Printf("Connected. Waiting for events.")
+	}
 	enc := json.NewEncoder(os.Stdout)
 	err = conn.Stream(context, func(stream bztcp.StreamData) {
 		err = enc.Encode(stream)
@@ -60,5 +79,9 @@ func main() {
 	})
 	if err != nil {
 		log.Fatalln(err)
+	}
+
+	if verbose {
+		log.Println("Finished. Time elapsed:", time.Since(startTime))
 	}
 }
